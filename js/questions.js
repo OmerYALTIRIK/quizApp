8807d2506c3072455d4d78dbb7c5b2d2cb0b383c
Omer YALTIRIK <omerfarukyaltirik@gmail.com>
2023-06-15 23:53:03 +0300

    15.06.23 uygulama başlangıcı

---
+++ js/questions.js
@@ -0,0 +1,42 @@
+const questions = {
+    "question1": {
+        "question": "Dünyanın en iyi yazılım dili hangisidir?",
+        "option1": "JavaScript",
+        "option2": "Java",
+        "option3": "C#",
+        "option4": "GO",
+        "answer": "JavaScript"
+    },
+    "question2": {
+        "question": "En güzel CSS framework hangisidir?",
+        "option1": "Tailwind",
+        "option2": "Bootstrap",
+        "option3": "PicoCSS",
+        "option4": "Bulma",
+        "answer": "Tailwind"
+    },
+    "question3": {
+        "question": "Hangi frontend framework'ü en iyisidir?",
+        "option1": "React",
+        "option2": "Vue",
+        "option3": "Angular",
+        "option4": "Svelte",
+        "answer": "React"
+    },
+    "question4": {
+        "question": "Hangi backend framework'ü en iyisidir?",
+        "option1": "Express",
+        "option2": "Django",
+        "option3": "Laravel",
+        "option4": "Spring",
+        "answer": "Express"
+    },
+    "question5": {
+        "question": "Hangi veritabanı en iyisidir?",
+        "option1": "MongoDB",
+        "option2": "MySQL",
+        "option3": "PostgreSQL",
+        "option4": "Redis",
+        "answer": "MongoDB"
+    },
+}
