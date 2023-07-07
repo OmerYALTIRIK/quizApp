4183698d4f0e194423c2eb31fd3fa81cacdfc3cc
Atilay <atovarkosker@gmail.com>
2023-07-07 19:16:48 +0300

    style: update code styling

--- js/script.js
+++ js/script.js
@@ -1,75 +1,78 @@
-const header = document.querySelector("header");
-const main = document.querySelector("main");
-const startButton = document.getElementById("startButton");
-const questionArea = document.querySelector(".questionsArea");
-const progress = document.querySelector("progress");
-const currentQuestion = document.getElementById("question");
-const answerButtons = document.querySelectorAll(".answerButton");
-const nextButton = document.querySelector(".nextButton");
-const scoreArea = document.querySelector(".scoreArea");
-const scoreButton = document.querySelector(".scoreButton");
-const app = document.querySelector(".app");
-
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const startButton = document.getElementById('startButton');
+const questionArea = document.querySelector('.questionsArea');
+const progress = document.querySelector('progress');
+const currentQuestion = document.getElementById('question');
+const answerButtons = document.querySelectorAll('.answerButton');
+const nextButton = document.querySelector('.nextButton');
+const scoreArea = document.querySelector('.scoreArea');
+const scoreButton = document.querySelector('.scoreButton');
+const app = document.querySelector('.app');
 
 const questions = [
   {
-    question: "Dünyanın en iyi yazılım dili hangisidir?",
-    options: ["JavaScript", "Java", "C#", "GO"],
-    answer: "JavaScript",
+    question: 'Dünyanın en iyi yazılım dili hangisidir?',
+    options: ['JavaScript', 'Java', 'C#', 'GO'],
+    answer: 'JavaScript',
   },
   {
-    question: "En güzel CSS framework hangisidir?",
-    options: ["Tailwind", "Bootstrap", "PicoCSS", "Bulma"],
-    answer: "Tailwind",
+    question: 'En güzel CSS framework hangisidir?',
+    options: ['Tailwind', 'Bootstrap', 'PicoCSS', 'Bulma'],
+    answer: 'Tailwind',
   },
   {
     question: "Hangi frontend framework'ü en iyisidir?",
-    options: ["React", "Vue", "Angular", "Svelte"],
-    answer: "React",
+    options: ['React', 'Vue', 'Angular', 'Svelte'],
+    answer: 'React',
   },
   {
     question: "Hangi backend framework'ü en iyisidir?",
-    options: ["Express", "Django", "Laravel", "Spring"],
-    answer: "Express",
+    options: ['Express', 'Django', 'Laravel', 'Spring'],
+    answer: 'Express',
+    //givenAnswer
   },
   {
-    question: "Hangi veritabanı en iyisidir?",
-    options: ["MongoDB", "MySQL", "PostgreSQL", "Redis"],
-    answer: "MongoDB",
+    question: 'Hangi veritabanı en iyisidir?',
+    options: ['MongoDB', 'MySQL', 'PostgreSQL', 'Redis'],
+    answer: 'MongoDB',
+    //givenAnswer
   },
 ];
+//
+//cevap verilince setLocalStorage(index,questions[])
+//getLocalStorage(index,questions[])
 
-window.onload = () => {
-  main.style.display = "none";
-  scoreArea.style.display = "none";
-};
-
-startButton.addEventListener("click", startQuiz);
-
-let currentQuestionIndex = 0;
+let currentQuestionIndex = 2;
 let score = 0;
 
-function startQuiz() {
-  header.style.display = "none";
-  main.style.display = "block";
+window.onload = () => {
+  main.style.display = 'none';
+  scoreArea.style.display = 'none';
+};
+const startQuiz = () => {
+  header.style.display = 'none';
+  main.style.display = 'block';
   showQuestion();
-}
+};
+startButton.addEventListener('click', startQuiz);
+scoreButton.addEventListener('click', showScore);
+nextButton.addEventListener('click', nextQuestion);
 
 function showQuestion() {
   const currentQuestionData = questions[currentQuestionIndex];
   currentQuestion.textContent = currentQuestionData.question;
   answerButtons.forEach((button, index) => {
+    //document.createelement
+    //appendChild
     button.textContent = currentQuestionData.options[index];
-    button.classList.remove("correct", "wrong");
+    button.classList.remove('correct', 'wrong');
     button.disabled = false;
+    button.addEventListener('click', checkAnswer);
   });
-  nextButton.style.display = "none";
+  nextButton.style.display = 'none';
   progress.value = ((currentQuestionIndex + 1) / questions.length) * 100;
 }
-
-answerButtons.forEach((button) => {
-  button.addEventListener("click", checkAnswer);
-});
 
 function checkAnswer(e) {
   const currentQuestionData = questions[currentQuestionIndex];
@@ -77,62 +80,58 @@ function checkAnswer(e) {
   const selectedAnswer = selectedButton.textContent;
 
   if (selectedAnswer === currentQuestionData.answer) {
-    selectedButton.classList.add("correct");
+    selectedButton.classList.add('correct');
+    selectedButton.style.backgroundColor = 'green';
     score++;
   } else {
-    selectedButton.classList.add("wrong");
+    selectedButton.classList.add('wrong');
+    selectedButton.style.backgroundColor = 'red';
+
     const correctButton = Array.from(answerButtons).find((button) => button.textContent === currentQuestionData.answer);
-    correctButton.classList.add("correct");
+    correctButton.classList.add('correct');
+    correctButton.style.backgroundColor = 'green';
   }
 
-  answerButtons.forEach((button) => {
-    button.disabled = true;
-  });
+  answerButtons.forEach((button) => (button.disabled = true));
 
-  nextButton.style.display = "block";
+  nextButton.style.display = 'block';
 }
-
-nextButton.addEventListener("click", nextQuestion);
 
 function nextQuestion() {
   currentQuestionIndex++;
-  if (currentQuestionIndex < questions.length) {
-    showQuestion();
-  } else {
-    showScoreButton();
-  }
+  answerButtons.forEach((button) => (button.style.backgroundColor = '#ff7f50'));
+  if (currentQuestionIndex < questions.length) return showQuestion();
+  showScoreButton();
 }
 
 function showScoreButton() {
-  nextButton.style.display = "none";
-  scoreArea.style.display = "block";
+  nextButton.style.display = 'none';
+  scoreArea.style.display = 'block';
 }
 
-scoreButton.addEventListener("click", showScore);
-
 function showScore() {
-  app.style.display = "none";
-  scoreArea.style.display = "block";
-  const scoreText = document.getElementById("scoreText");
+  app.style.display = 'none';
+  scoreArea.style.display = 'block';
+  const scoreText = document.getElementById('scoreText');
   scoreText.textContent = `Doğru: ${score} - Yanlış: ${questions.length - score}`;
 
-  const scoreList = document.getElementById("scoreList");
-  scoreList.innerHTML = "";
+  const scoreList = document.getElementById('scoreList');
+  scoreList.innerHTML = '';
 
   questions.forEach((question, index) => {
-    const listItem = document.createElement("li");
+    const listItem = document.createElement('li');
     const givenAnswer = answerButtons[index].textContent;
 
-    listItem.textContent = `${index + 1}. Soru: ${question.question} - Cevap: ${question.answer} - Verilen Cevap: ${givenAnswer}`;
+    listItem.textContent = `${index + 1}. Soru: ${question.question} - Cevap: ${
+      question.answer
+    } - Verilen Cevap: ${givenAnswer}`;
 
     if (givenAnswer === question.answer) {
-      listItem.classList.add("correct");
+      listItem.classList.add('correct');
     } else {
-      listItem.classList.add("wrong");
+      listItem.classList.add('wrong');
     }
 
     scoreList.appendChild(listItem);
   });
 }
-
-
